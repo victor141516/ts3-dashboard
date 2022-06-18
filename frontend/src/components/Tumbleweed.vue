348ae26b136dee5aa86bfb5ea3ffcd81cb7bab44
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-06-18 16:22:54 +0200

    feat: import tumbleweed animation

--- frontend/src/components/Tumbleweed.vue
+++ frontend/src/components/Tumbleweed.vue
@@ -32,43 +32,43 @@
 
 @keyframes tumbleweed {
   0% {
-    transform: translate(0vw, 45px) rotate(0deg);
+    transform: translate(calc(0vw - 100px), 45px) rotate(0deg);
   }
   9% {
-    transform: translate(9vw, 5px) rotate(130deg);
+    transform: translate(calc(9vw - 82px), 5px) rotate(130deg);
   }
   17% {
-    transform: translate(17vw, 45px) rotate(250deg);
+    transform: translate(calc(17vw - 66px), 45px) rotate(250deg);
   }
   26% {
-    transform: translate(26vw, 15px) rotate(360deg);
+    transform: translate(calc(26vw - 48px), 15px) rotate(360deg);
   }
   34% {
-    transform: translate(34vw, 45px) rotate(490deg);
+    transform: translate(calc(34vw - 32px), 45px) rotate(490deg);
   }
   42% {
-    transform: translate(42vw, 30px) rotate(600deg);
+    transform: translate(calc(42vw - 16px), 30px) rotate(600deg);
   }
   50% {
-    transform: translate(50vw, 45px) rotate(720deg);
+    transform: translate(calc(50vw + 0px), 45px) rotate(720deg);
   }
   59% {
-    transform: translate(59vw, 15px) rotate(830deg);
+    transform: translate(calc(59vw + 18px), 15px) rotate(830deg);
   }
   67% {
-    transform: translate(67vw, 45px) rotate(950deg);
+    transform: translate(calc(67vw + 34px), 45px) rotate(950deg);
   }
   76% {
-    transform: translate(76vw, 15px) rotate(1060deg);
+    transform: translate(calc(76vw + 52px), 15px) rotate(1060deg);
   }
   84% {
-    transform: translate(84vw, 45px) rotate(1180deg);
+    transform: translate(calc(84vw + 68px), 45px) rotate(1180deg);
   }
   93% {
-    transform: translate(93vw, 15px) rotate(1300deg);
+    transform: translate(calc(93vw + 86px), 15px) rotate(1300deg);
   }
   100% {
-    transform: translate(100vw, 45px) rotate(1420deg);
+    transform: translate(calc(100vw + 100px), 45px) rotate(1420deg);
   }
 }
 </style>
